9bc9147574e7bce676fdf0519211c66ef57b6aae
Dev avology <devavology12@gmail.com>
2024-07-12 10:19:19 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-    $(".menu-icon").click(function () {
-      $("body").addClass("menuToggle");
-    });
-    $(".close-icon").click(function () {
-      $("body").removeClass("menuToggle");
-    });
+  $(".menu-icon").click(function () {
+    $("body").addClass("menuToggle");
   });
+  $(".close-icon").click(function () {
+    $("body").removeClass("menuToggle");
+  });
+});
 
 function toggleText(moreId,buttonId){
-  var moreText=document.getElementById(moreId);
-  var buttonText=document.getElementById(buttonId);
-  
-  if(moreText.style.display==="none"){
-    moreText.style.display="block";
-    buttonText.innerHTML="Read Less";
-      }
-      else{
-        moreText.style.display="none";
-        buttonText.innerHTML="Read More";
-      }
+var moreText=document.getElementById(moreId);
+var buttonText=document.getElementById(buttonId);
+
+if(moreText.style.display==="none"){
+  moreText.style.display="block";
+  buttonText.innerHTML="Read Less";
+    }
+    else{
+      moreText.style.display="none";
+      buttonText.innerHTML="Read More";
+    }
 }
 
 
 $('.subpage-readmore-btn').click(function(e) {
-  e.preventDefault();
-  // console.log('click');
-  $(this).parent().prev().slideToggle('slow');
-  // $('.subpage-exp-content').slideToggle('slow');
-  $(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
+e.preventDefault();
+// console.log('click');
+$(this).parent().prev().slideToggle('slow');
+// $('.subpage-exp-content').slideToggle('slow');
+$(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
 });
 
 
